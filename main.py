a8f7862cd961f86ebb5585d55f3c6a3bccf4892a
Erwin Lejeune <erwin.lejeune15@gmail.com>
2019-11-23 18:30:24 +0100

    ⚡ add send email feature

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ list_ppl = []
 for i in range(nb_ppl):
     list_ppl.append(input("Enter person number " + str(i+1) + "'s name --> "))
 
+email_dict = dict()
+for elem in list_ppl:
+    email_dict[elem] = input("Enter " + elem + "'s EMAIL --> ")
+
 secret_dict = dict()
 list_match = list_ppl.copy()
 for elem in list_ppl:
@@ -21,7 +25,25 @@ for elem in list_ppl:
     list_match.remove(secret_santa)
 
 print(secret_dict)
+print(email_dict)
 
 # Email handle #
 
-sender = 
+sender = 'secretsanta.test1996@gmail.com'
+
+
+sender = "secretsanta.test1996@gmail.com"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+#Next, log in to the server
+server.login("secretsanta.test1996@gmail.com", "flifel85")
+
+#Send the mail
+for elem in list_ppl:
+    msg = """
+    Hey """ + elem + """, your secret santa is : """ 
+    + secret_dict[elem] 
+    server.sendmail(sender, email_dict[elem], msg)
+
+server.quit()
