a680edf200e456bea16516a1ad852d0e154b6025
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-15 16:51:12 +0100

    💡 document main file

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from lib.email_flush import Flush
 from lib.santa_gen import Santa
 import sys
+'''
+main script to generate secret santa pool, send emails to participant, flush the sent emails
+
+can't cheat ! hehe
+
+args :
+-email: Gmail email
+-pw: gmail pw
+
+example : python santa_gen.py email=toto@popo.com pw=password
+'''
 
 
 if __name__ == '__main__':
