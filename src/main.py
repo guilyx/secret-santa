b7d1b8df1cc37be1514b354c715bca5db37135c9
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-20 12:23:12 +0100

    📝 reverse doc

--- src/main.py
+++ src/main.py
@@ -1,10 +1,3 @@
-import sys
-from os import path
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-
-from lib.email_flush import Flush
-from lib.santa_gen import Santa
-
 '''
 main script to generate secret santa pool, send emails to participant, flush the sent emails
 
@@ -16,6 +9,14 @@ args :
 
 example : python main.py email=toto@popo.com pw=password
 '''
+
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from lib.email_flush import Flush
+#from lib.santa_gen import Santa
+
 
 
 if __name__ == '__main__':
@@ -32,7 +33,7 @@ if __name__ == '__main__':
     ss.send_emails()
 
     ##### Flushing #####
-    flush_sent = Flush(args['mail'], args['pw'])
+    flush_sent = Flush(args['mail'], args['pw'], ss.nb_ppl - 2)
 
     flush_sent.connectImap()
     flush_sent.deleteSentMails()
