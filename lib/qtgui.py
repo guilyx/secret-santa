aece1824852f82d8687fa240e5eb5108655dcc58
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-04-15 04:00:17 +0200

    Implement PySide GUI

--- lib/qtgui.py
+++ lib/qtgui.py
@@ -1,6 +1,8 @@
 import sys
 import random
+import time
 from PySide2 import QtCore, QtWidgets, QtGui
+from santa_gen import Santa
 
 
 class MyWidget(QtWidgets.QWidget):
@@ -18,6 +20,8 @@ class MyWidget(QtWidgets.QWidget):
         self.groupBoxes = dict()
         self.sliders = dict()
         self.entries = dict()
+        self.list = dict()
+        self.layouts = []
 
         self.defaultFont = QtGui.QFont('Helvetica', 11, QtGui.QFont.Normal)
 
@@ -31,32 +35,37 @@ class MyWidget(QtWidgets.QWidget):
 
     def __clearLayout(self, layout):
         for i in reversed(range(layout.count())):
-            layout.itemAt(i).widget().setParent(None)
+            if layout.itemAt(i).widget() == None:
+                continue
+            else:
+                layout.itemAt(i).widget().setParent(None)
 
     def __firstMenu(self):
-        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.mainLayout = QtWidgets.QVBoxLayout(self)
+        self.layouts.append(self.mainLayout)
         self.mainLayout.setContentsMargins(20, 20, 20, 20)
 
         self.__createProgressBar('Progress', 3)
         self.__setButtons()
         self.__setSanta()
 
-        introText = "Author : Erwin Lejeune\nDate of Creation : 14/04/2020\nLicense: MIT"
-        self.__createLabel(introText, QtCore.Qt.AlignJustify)
-        self.labels[introText].setWordWrap(True)
+        self.introText = "Author : Erwin Lejeune\nDate of Creation : 14/04/2020\nLicense: MIT"
+        self.__createLabel(self.introText, QtCore.Qt.AlignJustify)
+        self.labels[self.introText].setWordWrap(True)
 
         self.groupBoxes['Secret Santa App'].setLayout(self.mainLayout)
-        self.mainLayout.addWidget(self.labels[introText])
+        self.mainLayout.addWidget(self.labels[self.introText])
         self.mainLayout.addLayout(self.applicationLayout)
         self.mainLayout.addWidget(self.groupBoxes['Options'])
         self.mainLayout.addWidget(self.groupBoxes['Progress'])
 
-        self.buttons['Run'].clicked.connect(self.__run)
+        self.buttons['Next'].clicked.connect(self.__run)
         self.buttons['Exit'].clicked.connect(self.__exitUI)
         self.buttons['About'].clicked.connect(self.__aboutBox)
 
     def __setSanta(self):
         self.applicationLayout = QtWidgets.QHBoxLayout()
+        self.layouts.append(self.applicationLayout)
         self.__createGroupBox('Gmail IDs', self.groupBoxes['Secret Santa App'])
         self.__createGroupBox('Pool Size', self.groupBoxes['Secret Santa App'])
 
@@ -64,8 +73,12 @@ class MyWidget(QtWidgets.QWidget):
         self.__createTextEntry('Gmail IDs')
         self.__createTextEntry(
             'Password', echoMode=QtWidgets.QLineEdit.Password)
-            
+
+        self.entryInit = [self.entries['Gmail IDs'].text(),
+                          self.entries['Password'].text()]
+
         self.nparticipantsLayout = QtWidgets.QHBoxLayout()
+        self.layouts.append(self.nparticipantsLayout)
         self.nparticipantsLayout.addWidget(self.labels['Pool Size'])
         self.nparticipantsLayout.addWidget(self.sliders['Pool Size'])
         self.groupBoxes['Pool Size'].setLayout(self.nparticipantsLayout)
@@ -79,19 +92,126 @@ class MyWidget(QtWidgets.QWidget):
         self.applicationLayout.addWidget(self.groupBoxes['Pool Size'])
 
     def __run(self):
-        if (self.entries['Gmail IDs'].text() is not '') and (self.entries['Password'].text() is not ''):
-            QtWidgets.QMessageBox.information(
-                self.buttons['Run'], 'Information', 'You can now enter the names and emails of your list.')
-        else:
+        if (self.entries['Gmail IDs'].text() is '') or (self.entries['Password'].text() is ''):
             QtWidgets.QMessageBox.critical(
-                self.buttons['Run'], 'Error', 'You need to enter your gmail ids !')
+                self.buttons['Next'], 'Error', 'You need to enter your gmail ids !')
+
+        elif (self.entries['Gmail IDs'].text() is self.entryInit[0]) or (self.entries['Password'].text() is self.entryInit[1]):
+            QtWidgets.QMessageBox.critical(
+                self.buttons['Next'], 'Error', 'You need to enter your gmail ids !')
+
+        else:
+            self.gmailId = self.entries['Gmail IDs'].text()
+            self.gmailPwd = self.entries['Password'].text()
+
+            QtWidgets.QMessageBox.information(
+                self.buttons['Next'], 'Information', 'You can now enter the names and emails of your list.')
+
+            self.nparticipants = self.sliders['Pool Size'].value()
+
+            self.__clearLayout(self.applicationLayout)
+            self.__clearLayout(self.nparticipantsLayout)
+            self.__clearLayout(self.nameEntriesLayout)
+
+            for o in range(10):
+                self.progressBars['Progress'].setValue(
+                    self.progressBars['Progress'].value() + random.randint(1, 3))
+                time.sleep(0.1)
+            self.poolSize = self.sliders['Pool Size'].value()
+            self.__layoutSantaList()
+
+    def __layoutSantaList(self):
+        self.buttons['Next'].clicked.disconnect()
+        self.__createGroupBox('Pool Identifiers',
+                              self.groupBoxes['Secret Santa App'])
+        self.idLayout = QtWidgets.QGridLayout()
+
+        for i in range(self.poolSize):
+            self.__createTextEntry('Name' + str(i))
+            self.__createTextEntry('Email' + str(i))
+            self.idLayout.addWidget(self.entries['Name' + str(i)], i, 0)
+            self.idLayout.addWidget(self.entries['Email' + str(i)], i, 1)
+
+        self.groupBoxes['Pool Identifiers'].setLayout(self.idLayout)
+        self.__clearLayout(self.mainLayout)
+
+        self.scrollArea = QtWidgets.QScrollArea()
+        #self.scrollArea.setFixedWidth(560)
+        self.scrollArea.setWidgetResizable(True)
+        #self.scrollArea.setBackgroundRole(QtGui.QPalette.Dark)
+        self.scrollArea.setWidget(self.groupBoxes['Pool Identifiers'])
+
+        self.mainLayout.addWidget(self.labels[self.introText])
+        #self.mainLayout.addWidget(self.groupBoxes['Pool Identifiers'])
+        self.mainLayout.addWidget(self.scrollArea)
+        self.buttons['Next'].setText('Run')
+        self.buttons['Run'] = self.buttons['Next']
+        self.buttons.pop('Next')
+        self.mainLayout.addWidget(self.groupBoxes['Options'])
+        self.mainLayout.addWidget(self.groupBoxes['Progress'])
+
+        self.buttons['Run'].clicked.connect(self.__generatePool)
+
+    def __generatePool(self):
+        self.secretNames = []
+        self.secretEmails = dict()
+        for i in range(self.nparticipants):
+            name = self.entries['Name' + str(i)].text()
+            self.secretNames.append(name)
+            self.secretEmails[name] = self.entries['Email' + str(i)].text()
+
+        secretSanta = Santa(self.gmailId, self.gmailPwd)
+
+        self.labels['Progress'].setText('Generating random pool...')
+        for o in range(10):
+            self.progressBars['Progress'].setValue(
+                self.progressBars['Progress'].value() + random.randint(1, 3))
+            time.sleep(0.1)
+
+        secretSanta.set_number(self.nparticipants)
+        secretSanta.set_names(self.secretNames)
+        secretSanta.set_emails(self.secretEmails)
+        secretSanta.gen_secrets()
+
+        self.labels['Progress'].setText('Sending Emails...')
+
+        for o in range(10):
+            self.progressBars['Progress'].setValue(
+                self.progressBars['Progress'].value() + random.randint(1, 3))
+            time.sleep(0.1)
+
+        secretSanta.send_emails()
+        timestamp = time.time()
+        secretSanta.flush_emails()
+
+        self.labels['Progress'].setText('Flushing proofs...')
+
+        for o in range(10):
+            self.progressBars['Progress'].setValue(
+                self.progressBars['Progress'].value() + random.randint(1, 3))
+            time.sleep(0.01)
+
+        self.gmailId = None
+        self.gmailPwd = None
+
+        self.labels['Progress'].setText('Exiting cleanly...')
+
+        for o in range(10):
+            self.progressBars['Progress'].setValue(
+                self.progressBars['Progress'].value() + random.randint(1, 3))
+            time.sleep(0.01)
+
+        while time.time() - timestamp < 10:
+            pass
+
+        self.labels['Progress'].setText('Complete !')
 
     def __createSlider(self, label):
         self.sliders[label] = QtWidgets.QSlider()
         self.sliders[label].setOrientation(QtCore.Qt.Horizontal)
         self.sliders[label].setTickInterval(1)
         self.sliders[label].setMinimum(3)
-        self.sliders[label].setMaximum(100)
+        self.sliders[label].setMaximum(30)
         self.labels[label] = QtWidgets.QLabel('3')
 
         self.sliders[label].valueChanged.connect(self.sliderSignalValue)
@@ -152,7 +272,7 @@ class MyWidget(QtWidgets.QWidget):
         # Create label
         self.__createGroupBox('Progress', self.groupBoxes['Secret Santa App'])
 
-        progressLayout = QtWidgets.QHBoxLayout()
+        self.progressLayout = QtWidgets.QHBoxLayout()
 
         # Create Progress Bar widget
         self.progressBars[label] = QtWidgets.QProgressBar()
@@ -160,14 +280,18 @@ class MyWidget(QtWidgets.QWidget):
         self.progressBars[label].setMinimum(0)
         self.progressBars[label].setMaximum(100)
 
+        # Create label
+        self.labels[label] = QtWidgets.QLabel('Configuration...')
+
         # Attach Progress to new Status
         self.statusBars[label] = QtWidgets.QStatusBar()
         self.progressBars[label].setValue(value)  # function here
-        self.statusBars[label].addWidget(self.progressBars[label], 1)
+        self.statusBars[label].addWidget(self.labels[label], 1)
+        self.statusBars[label].addWidget(self.progressBars[label], 2)
 
-        progressLayout.addWidget(self.statusBars[label])
+        self.progressLayout.addWidget(self.statusBars[label])
 
-        self.groupBoxes['Progress'].setLayout(progressLayout)
+        self.groupBoxes['Progress'].setLayout(self.progressLayout)
 
     def __createStatusBar(self, message, time_ms):
         self.statusBars[message] = QtWidgets.QStatusBar()
@@ -181,17 +305,17 @@ class MyWidget(QtWidgets.QWidget):
 
     def __setButtons(self):
         self.__createGroupBox('Options', self.groupBoxes['Secret Santa App'])
-        buttonsLayout = QtWidgets.QHBoxLayout()
+        self.buttonsLayout = QtWidgets.QHBoxLayout()
 
-        self.__createButton('Run')
+        self.__createButton('Next')
         self.__createButton('Exit')
         self.__createButton('About')
 
-        buttonsLayout.addWidget(self.buttons['Run'])
-        buttonsLayout.addWidget(self.buttons['About'])
-        buttonsLayout.addWidget(self.buttons['Exit'])
+        self.buttonsLayout.addWidget(self.buttons['Next'])
+        self.buttonsLayout.addWidget(self.buttons['About'])
+        self.buttonsLayout.addWidget(self.buttons['Exit'])
 
-        self.groupBoxes['Options'].setLayout(buttonsLayout)
+        self.groupBoxes['Options'].setLayout(self.buttonsLayout)
 
     def __aboutBox(self):
         QtWidgets.QMessageBox.about(self.buttons['About'], "About Secret Santapp",
